07aa4bb88d9620e4db9e48aa35b5c8006b334e0f
billiam <hello@billiam.io>
2017-10-25 20:12:56 -0700

    ch4 asm complete

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,14 +1,71 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+// NOTE infinite loop test
+//  @SCREEN
+//  D=A
+//(LOOP)
+//  A=D
+//  M=-1
+//  D=A+1
+//  @LOOP
+//  0;JMP
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+  @24575
+  D=A
+  @max_screen
+  M=D
 
-// Put your code here.
+  @SCREEN // 16384
+  D=A
+  @pointer
+  M=D
+
+(LOOP)
+  @KBD // 24576
+  D=M
+  @UNFILL
+  D;JEQ // if no keyboard, unfill
+  @FILL
+  0;JMP // else fill
+
+(UNFILL)
+  @pointer
+  A=M
+  M=0
+  @TILLBAKA
+  0;JMP
+
+(FILL)
+  @pointer
+  A=M
+  M=-1
+  @FRAMOAT
+  0;JMP
+
+(FRAMOAT)
+  @pointer
+  D=M
+  @max_screen
+  D=D-A
+  @LOOP
+  D;JEQ
+
+  @pointer
+  D=M
+  D=D+1
+  M=D
+  @LOOP
+  0;JMP
+
+(TILLBAKA)
+  @pointer
+  D=M
+  @SCREEN
+  D=D-A
+  @LOOP
+  D;JEQ
+
+  @pointer
+  D=M
+  D=D-1
+  M=D
+  @LOOP
+  0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+  @R2 // init R2 to 0
+  M=0
+
+  @R1 // get R1 to use as counter
+  D=M
+  @R3 // save counter to R3
+  M=D
+(LOOP)
+  @R3
+  D=M
+  @END
+  D;JEQ // if counter is zero, END
+
+  @R2
+  D=M
+  @R0
+  D=D+M
+  
+  @R2 // save to R2
+  M=D
+
+  @R3 // decrement counter
+  D=M-1
+  M=D
+
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
